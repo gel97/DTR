b1def36c21997e56fb26d64390e04d8b88f0c601
Rogel Alangilan <alangilan.rogel@gmail.com>
2026-02-09 14:06:51 +0800

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layout/main-layout/main-layout';
+import { Home } from './features/home/home';
+import { home_routes } from './features/home/home.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      ...home_routes
+    ]
+  }
+];
---
+++ src/app/features/home/home.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Home } from './home';
+
+export const home_routes: Routes = [
+    {
+        path: 'home',
+        component: Home,
+    }
+];
